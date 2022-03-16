8d521a5cb51dec667f3ec676654e675f4e1c3c93
rivka123 <gasasa255@gmail.com>
2022-03-16 15:44:55 +0200

    list exs

--- print/ex print.py
+++ print/ex print.py
@@ -1,47 +1,54 @@
-# 1
-a=int(input("please enter number:"))
-print(pow(a,2))
+# # 1
+# a=int(input("please enter number:"))
+# print(pow(a,2))
+#
+# # 2
+# age=int(input("please enter your age:"))
+# print(age+20)
+#
+# # 3
+# price=int(input("please enter price:"))
+# print(price*1.17)
+#
+# # 4
+# num1=int(input("enter number:"))
+# num2=int(input("enter number:"))
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+#
+# # 5
+# print("welcome")
+# print("09-03-22 Wednesday")
+#
+# # 6
+# num1=int(input("enter number:"))
+# num2=int(input("enter number:"))
+# num3=int(input("enter number:"))
+# num4=int(input("enter number:"))
+# print(num4+num3+num2+num1/4)
+#
+# # 7
+# nail=int(input("enter number of nail:"))
+# print(nail/40)
+#
+# # 8
+# kilo=int(input("please enter kilometers:"))
+# speed=int(input("please enter speed:"))
+# print(kilo/speed)
+#
+# # 9
+# num=int(input("please enter your phon number:"))
+# print("the number is:",end='')
+# print("0",num)
 
-# 2
-age=int(input("please enter your age:"))
-print(age+20)
 
-# 3
-price=int(input("please enter price:"))
-print(price*1.17)
 
-# 4
-num1=int(input("enter number:"))
-num2=int(input("enter number:"))
-print(num1+num2)
-print(num1-num2)
-print(num1*num2)
-print(num1/num2)
 
-# 5
-print("welcome")
-print("09-03-22 Wednesday")
 
-# 6
-num1=int(input("enter number:"))
-num2=int(input("enter number:"))
-num3=int(input("enter number:"))
-num4=int(input("enter number:"))
-print(num4+num3+num2+num1/4)
 
-# 7
-nail=int(input("enter number of nail:"))
-print(nail/40)
 
-# 8
-kilo=int(input("please enter kilometers:"))
-speed=int(input("please enter speed:"))
-print(kilo/speed)
-
-# 9
-num=int(input("please enter your phon number:"))
-print("the number is:",end='')
-print("0",num)
 
 
 
