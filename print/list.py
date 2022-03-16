46408ea887073a31b537557ca96d6fd496dbd10b
rivka123 <gasasa255@gmail.com>
2022-03-16 15:45:54 +0200

    list exs

--- print/list.py
+++ print/list.py
@@ -1,48 +1,48 @@
 # 1
-# print("i love python"*5)
+print("i love python"*5)
 
 # 2
-# numbers= [1,2,3,4,5,6,7,8,9,10]
-# print(numbers)
+numbers= [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
 
 # 3
-# names =["rivka","simha","salmon","eli","yonatan","miki","betty","aviva","oshrat","yemikir"]
-# print(names)
-# x=sorted(names)
-# print(x)
+names =["rivka","simha","salmon","eli","yonatan","miki","betty","aviva","oshrat","yemikir"]
+print(names)
+x=sorted(names)
+print(x)
 
 # 4
-# x = [10,45,88,78,64,98,23,56,44,12,3,]
-# print(sum(x))
-# print(x[0]/11)
-# print(x[1]/11)
-# print(x[2]/11)
-# print(x[3]/11)
-# print(x[4]/11)
-# print(x[5]/11)
-# print(x[6]/11)
-# print(x[7]/11)
-# print(x[8]/11)
-# print(x[9]/11)
-# print(x[10]/11)
+x = [10,45,88,78,64,98,23,56,44,12,3,]
+print(sum(x))
+print(x[0]/11)
+print(x[1]/11)
+print(x[2]/11)
+print(x[3]/11)
+print(x[4]/11)
+print(x[5]/11)
+print(x[6]/11)
+print(x[7]/11)
+print(x[8]/11)
+print(x[9]/11)
+print(x[10]/11)
 
-# for i in x:
-#     if i > 10:
-#         print(i)
+for i in x:
+    if i > 10:
+        print(i)
 
-# for i in x:
-#     if i < 10:
-#         print(i)
+for i in x:
+    if i < 10:
+        print(i)
 
-# print(max(x))
+print(max(x))
 
-# for i in x:
-#     if i % 2 == 0:
-#         print(i)
+for i in x:
+    if i % 2 == 0:
+        print(i)
 
-# for i in x:
-#     if i % 3 == 0:
-#         print(i)
+for i in x:
+    if i % 3 == 0:
+        print(i)
 
 
 
