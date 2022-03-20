743cf26d146ac5a78569f11954e8eea3cc0957d6
rivka123 <gasasa255@gmail.com>
2022-03-20 20:52:54 +0200

    exs

--- basic exs/func.py
+++ basic exs/func.py
@@ -1,37 +1,37 @@
-# # 1
-# def rivka (x,y):
-#    print(x+y)
-#
-# print(rivka(15,25))
+# 1
+def plus (x,y):
+   print(x+y)
 
-# # 2
-# def rivka (x,y):
-#    z =(x+y)
-#    return z
-#
-# # call function
-# s =rivka(5,10)
-# print(s)
+print(plus(15,25))
 
-# 3
-# def notgetandnotreternfunc():
-#     print("hi from ... ")
-#
-# def getandnotretern(x):
-#     print(x)
-#
-# def notgetandreterunfunc():
-#     return "hi rivka ,,,, "
-#
-# def getandreturnfunc(x, y):
-#     return x * y
-#
-# notgetandnotreternfunc()
-# getandnotretern(8)
-# v = notgetandreterunfunc()
-# print(v)
-# a=getandreturnfunc(7,3)
-# print(a)
+# 2
+def rivka (x,y):
+   z =(x+y)
+   return z
+
+# call function
+s =rivka(5,10)
+print(s)
+
+3
+def notgetandnotreternfunc():
+    print("hi from ... ")
+
+def getandnotretern(x):
+    print(x)
+
+def notgetandreterunfunc():
+    return "hi rivka ,,,, "
+
+def getandreturnfunc(x, y):
+    return x * y
+
+notgetandnotreternfunc()
+getandnotretern(8)
+v = notgetandreterunfunc()
+print(v)
+a=getandreturnfunc(7,3)
+print(a)
 
 
 
