2841a242859bebd4c1256d79ed002f459bb18697
rivka123 <gasasa255@gmail.com>
2022-03-21 19:09:03 +0200

    exs

--- basic exs/func.py
+++ basic exs/func.py
@@ -33,6 +33,105 @@ print(v)
 a=getandreturnfunc(7,3)
 print(a)
 
+# 4
+def countnumbers (ls):
+    newls=[0,0,0,0,0,0,0,0,0,0]
+    for i in ls:
+        newls[i] += 1
+    return newls
+
+ls = [1,1,2,7,3,5,8,9]
+
+newls = countnumbers(ls)
+print(newls)
+
+5
+def avg ():
+    sun = 0
+    count = 0
+    num = input("enter a number : ")
+    while num != q:
+        count +=1
+        sum = int(num)
+        num = input("enter a number : ")
+
+    print(sun/count)
+avg()
+
+
+# 6
+def palindrom(x):
+start = 0
+end = len(st)-1
+for i in range(len(st)//2):
+    if str(start) != st(end):
+        print("not palindrom")
+        return
+    else:
+        start = 1
+        end = 1
+print("palindrom")
+palindrom("abba")
+
+# 7
+def minute(x):
+    print(x / 60)
+min = int(input("please enter a minutes :"))
+minute(min)
+
+# 8
+def posorneg(ls):
+    for i in ls:
+        if i > 0:
+            print("Positive number")
+        if i == float or i < 0:
+            print("negative number")
+
+ls = [8,9,-1,5,16.3,28,77,98]
+posorneg(ls)
+
+# 9
+def evenorodd (x):
+    if x % 2 == 0 :
+        print("0")
+    else:
+        print("1")
+num = int(input("enter a number: "))
+evenorodd(num)
+
+# 10
+num = str(input("enter a number whit 6 digit : "))
+even = 0
+odd = 0
+for i in (num):
+    a=int(i)
+    if a%2==0:
+        even+=1
+    else:
+        odd+=1
+print(even,"even number")
+print(odd,"odd number ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- basic exs/list.py
+++ basic exs/list.py
@@ -1,123 +1,126 @@
-# # 1
-# print("i love python"*5)
-#
-# # 2
-# numbers= [1,2,3,4,5,6,7,8,9,10]
-# print(numbers)
-#
-# # 3
-# names =["rivka","simha","salmon","eli","yonatan","miki","betty","aviva","oshrat","yemikir"]
-# print(names)
-# x=sorted(names)
-# print(x)
-#
-# # # 4
-# x = [10,45,88,78,64,98,23,56,44,12,3,]
-# print(sum(x))
-#
-# for j in x:
-#     print(j/11)
-#
-# for i in x:
-#     if i > 10:
-#         print("bigger then 10:" , i)
-#
-# for i in x:
-#     if i < 10:
-#         print("smaller then 10:" , i)
-#
-# print(max(x))
-#
-# for i in x:
-#     if i % 2 == 0:
-#         print(i)
-#
-# for i in x:
-#     if i % 3 == 0:
-#         print(i)
-#
-# # 5
-# data =[1,2,8,9,6,20,6,18,28,29,30]
-# def consecutives(data):
-#     start = 0
-#     i = start
-#     while i < len(data) - 1:
-#         if data[i] + 1 == data[i + 1]:
-#             i += 1
-#         else:
-#             yield(start, i - start + 1)
-#             start = i + 1
-#             i = start
-#     yield(start, i- start + 1)
-#
-# for seq in consecutives(data):
-#     print(seq)
-#
-# data.insert(0,3)
-# print(data)
-#
-# num = int(input("please enter a number: "))
-# data.insert(1,num)
-# print(data)
-#
-# x = data[1]+data[2]
-# data.insert(3,x)
-# print(data)
-#
-# x= data[0]*data[1]*data[2]
-# data.insert(4,x)
-# print(data)
-#
-# print(data)
-#
-# # grade
-# l1=[]
-# l2=[]
-# for i in range(5):
-#     l1.append(int(input("please enter your grade:")))
-#     l2.append(int(input("please enter your grade:")))
-#
-# for i in l1:
-#     for j in l2:
-#         if i == j:
-#             print(i , "grade in both")
+# 1
+print("i love python"*5)
 
-# # shvilisrael
-# shvil = []
-# sum = 0
-# for i in range(3):
-#     name = input("enter a name: ")
-#     dist = int(input("enter a length: "))
-#     shvil.append([dist])
-#
-#     if dist > 10:
-#         sum += dist
-#         print(dist)
-#
-# print("sum of shvil : ", sum)
+# 2
+numbers= [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
 
-# # 8
-# sum=0
-# for i in range(5):
-#     fname=input("enter a last name : ")
-#     kids=int(input("enter number of kids : "))
-#     print(fname, kids)
-#     if kids>3:
-#         print("DISCOUNT")
-#     sum += kids
-#
-# print(sum)
+# 3
+names =["rivka","simha","salmon","eli","yonatan","miki","betty","aviva","oshrat","yemikir"]
+print(names)
+x=sorted(names)
+print(x)
 
-# # 9 להמשיך
-# o = 0
-# sum = 0
-# for i in range(3):
-#     name=input("please enter your name : ")
-#     year=int(input("please enter your birth year : "))
-#     blood=input("please enter your blood type : ")
-#     print(name,year,blood)
-# if blood == o:
-# print(count(o))
+# # 4
+x = [10,45,88,78,64,98,23,56,44,12,3,]
+print(sum(x))
 
+for j in x:
+    print(j/11)
 
+for i in x:
+    if i > 10:
+        print("bigger then 10:" , i)
+
+for i in x:
+    if i < 10:
+        print("smaller then 10:" , i)
+
+print(max(x))
+
+for i in x:
+    if i % 2 == 0:
+        print(i)
+
+for i in x:
+    if i % 3 == 0:
+        print(i)
+
+# 5
+data =[1,2,8,9,6,20,6,18,28,29,30]
+def consecutives(data):
+    start = 0
+    i = start
+    while i < len(data) - 1:
+        if data[i] + 1 == data[i + 1]:
+            i += 1
+        else:
+            yield(start, i - start + 1)
+            start = i + 1
+            i = start
+    yield(start, i- start + 1)
+
+for seq in consecutives(data):
+    print(seq)
+
+data.insert(0,3)
+print(data)
+
+num = int(input("please enter a number: "))
+data.insert(1,num)
+print(data)
+
+x = data[1]+data[2]
+data.insert(3,x)
+print(data)
+
+x= data[0]*data[1]*data[2]
+data.insert(4,x)
+print(data)
+
+print(data)
+
+# grade
+l1=[]
+l2=[]
+for i in range(5):
+    l1.append(int(input("please enter your grade:")))
+    l2.append(int(input("please enter your grade:")))
+
+for i in l1:
+    for j in l2:
+        if i == j:
+            print(i , "grade in both")
+
+# shvilisrael
+shvil = []
+sum = 0
+for i in range(3):
+    name = input("enter a name: ")
+    dist = int(input("enter a length: "))
+    shvil.append([dist])
+
+    if dist > 10:
+        sum += dist
+        print(dist)
+
+print("sum of shvil : ", sum)
+
+# 8
+sum=0
+for i in range(5):
+    fname=input("enter a last name : ")
+    kids=int(input("enter number of kids : "))
+    print(fname, kids)
+    if kids>3:
+        print("DISCOUNT")
+    sum += kids
+
+print(sum)
+
+# 9 להמשיך
+o = 0
+sum = 0
+for i in range(3):
+    name=input("please enter your name : ")
+    year=int(input("please enter your birth year : "))
+    blood=input("please enter your blood type : ")
+    print(name,year,blood)
+if blood == o:
+print(count(o))
+
+# 10
+word = input("enter some word :")
+word_list = list(word)
+print(word_list)
 
